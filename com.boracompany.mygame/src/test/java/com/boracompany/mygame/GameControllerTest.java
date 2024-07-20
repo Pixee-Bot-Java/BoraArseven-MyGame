d6fc6e293da1391ebf13fcefc186ec37b68a1070
Bora Arseven <bora.arseven@gmail.com>
2024-07-20 19:27:16 +0200

    Added test class for gamecontroller, deleted test class for app

---
+++ com.boracompany.mygame/src/test/java/com/boracompany/mygame/GameControllerTest.java
@@ -0,0 +1,15 @@
+package com.boracompany.mygame;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameControllerTest {
+	
+
+	@Test
+	void test() {
+		assertEquals(true,true);
+	}
+
+}
