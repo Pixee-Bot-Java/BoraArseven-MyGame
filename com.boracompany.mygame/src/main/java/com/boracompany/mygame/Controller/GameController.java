4eb9c36007920f5bf67283236a2113438244a7e8
Bora Arseven <bora.arseven@gmail.com>
2024-07-20 19:26:10 +0200

    renamed App to GameContoller

---
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/Controller/GameController.java
@@ -0,0 +1,12 @@
+package com.boracompany.mygame.Controller;
+
+import com.boracompany.mygame.Model.Player;
+
+public class GameController {
+
+
+	void attack (Player attacker, Player defender) {
+	
+		defender.setHealth(0);
+	}
+}
