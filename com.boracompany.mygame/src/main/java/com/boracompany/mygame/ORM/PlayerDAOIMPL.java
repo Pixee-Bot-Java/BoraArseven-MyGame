a8ddcc7b88b3872ebe7eb08fe05523e115d53e85
Bora Arseven <bora.arseven@gmail.com>
2024-08-01 17:29:08 +0200

    Created PlayerDAOImpl class

---
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/ORM/PlayerDAOIMPL.java
@@ -0,0 +1,34 @@
+package com.boracompany.mygame.ORM;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.boracompany.mygame.Model.Player;
+
+
+public class PlayerDAOIMPL implements PlayerDAO{
+
+	@Override
+	public List<Player> getAllPlayers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Player getPlayer(int ID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updatePlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePlayer(Player player) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
