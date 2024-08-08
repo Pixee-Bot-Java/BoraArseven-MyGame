161483e927181eae08f3e0300908d5e2330b683f
Bora Arseven <bora.arseven@gmail.com>
2024-08-08 15:34:26 +0200

    Added log4j support

--- com.boracompany.mygame/src/main/java/com/boracompany/mygame/Controller/GameController.java
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/Controller/GameController.java
@@ -1,11 +1,15 @@
 package com.boracompany.mygame.Controller;
 
 import com.boracompany.mygame.Model.Player;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class GameController {
-
+	
+	private static final Logger LOGGER = LogManager.getLogger(GameController.class);
 
 //	void attack (Player attacker, Player defender) {
 //		defender.setHealth(0);
 //	}
+
+
 }
--- com.boracompany.mygame/src/main/java/com/boracompany/mygame/Main/Main.java
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/Main/Main.java
@@ -1,12 +1,17 @@
 package com.boracompany.mygame.Main;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import com.boracompany.mygame.Controller.GameController;
 
 public class Main {
+	private static final Logger LOGGER = LogManager.getLogger(GameController.class);
 	// this will activate view in future
 	public static void main(String[] args) {
+		LOGGER.info("App started");
 
 		System.out.println("Hello World!");
-		
+		LOGGER.info("App Started");
 	}
 	
 }
