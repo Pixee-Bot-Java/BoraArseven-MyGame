b38de6d8264bb7508c9f115620e65ef7b913cc4e
Bora Arseven <bora.arseven@gmail.com>
2024-08-10 17:55:37 +0200

    removed unnecessary setters, slightly translated playerdao methods

--- com.boracompany.mygame/src/main/java/com/boracompany/mygame/ORM/PlayerDAOIMPL.java
+++ com.boracompany.mygame/src/main/java/com/boracompany/mygame/ORM/PlayerDAOIMPL.java
@@ -8,99 +8,112 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.boracompany.mygame.Controller.GameController;
 import com.boracompany.mygame.Model.Player;
 
 public class PlayerDAOIMPL implements PlayerDAO {
+	
+	private static final Logger LOGGER = LogManager.getLogger(PlayerDAOIMPL.class);
+	private EntityManagerFactory emf;
+	
+	private EntityManager em;
+	
 
-    private EntityManagerFactory emf;
-    
-    
-    @Generated("exclude-from-coverage")
-    public EntityManagerFactory getEmf() {
+
+	@Generated("exclude-from-coverage")
+	public EntityManagerFactory getEmf() {
 		return emf;
 	}
 
-    @Generated("exclude-from-coverage")
-	public void setEmf(EntityManagerFactory emf) {
+
+	public PlayerDAOIMPL(EntityManagerFactory emf) {
 		this.emf = emf;
 	}
 
-	public PlayerDAOIMPL(EntityManagerFactory emf) {
-        this.emf = emf;
-    }
+	@Override
+	public List<Player> getAllPlayers() {
+		em = emf.createEntityManager();
+		List<Player> players = null;
+		try {
+			TypedQuery<Player> query = em.createQuery("SELECT p FROM Player p", Player.class);
+			players = query.getResultList();
+		} finally {
+			em.close();
+		}
+		return players;
+	}
 
-    @Override
-    public List<Player> getAllPlayers() {
-        EntityManager em = emf.createEntityManager();
-        List<Player> players = null;
-        try {
-            TypedQuery<Player> query = em.createQuery("SELECT p FROM Player p", Player.class);
-            players = query.getResultList();
-        } finally {
-            em.close();
-        }
-        return players;
-    }
+	@Override
+	public Player getPlayer(Long ID) {
+		em = emf.createEntityManager();
+		Player player = null;
+		try {
+			player = em.find(Player.class, ID);
+		} finally {
+			em.close();
+		}
+		return player;
+	}
 
-    @Override
-    public Player getPlayer(Long ID) {
-        EntityManager em = emf.createEntityManager();
-        Player player = null;
-        try {
-            player = em.find(Player.class, ID);
-        } finally {
-            em.close();
-        }
-        return player;
-    }
+	@Override
+	public void updatePlayer(Player player) throws IllegalStateException {
+		em = emf.createEntityManager();
+		EntityTransaction transaction = null;
+		try {
+			transaction = em.getTransaction();
+			if ((transaction != null)) {
+				transaction.begin();
+				em.merge(player);
+				transaction.commit();
+			}
+			// Handled NullpointerException thanks to jacoco's guidance.
+			else {
+				throw new IllegalStateException("Transaction is null");
+			}
+		} catch (RuntimeException e) {
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+			throw e;
+		}
 
-    @Override
-    public void updatePlayer(Player player) {
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction transaction = null;
-        try {
-        	
-            transaction = em.getTransaction();
-            if((transaction != null)) {
-            	 transaction.begin();
-                 em.merge(player);
-                 transaction.commit();
-            }
-            // Handled NullpointerException thanks to jacoco's guidance.
-            else {
-            	 throw new IllegalStateException("Transaction is null");
-            }
-           
-        } catch (RuntimeException e) {
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-            throw e;
-        } finally {
-            em.close();
-        }
-    }
+		finally {
+			em.close();
+		}
+	}
 
-    @Override
-    public void deletePlayer(Player player) {
-        EntityManager em = emf.createEntityManager();
-        EntityTransaction transaction = null;
-        try {
-            transaction = em.getTransaction();
-            transaction.begin();
-            Player managedPlayer = em.find(Player.class, player.getId());
-            if (managedPlayer != null) {
-                em.remove(managedPlayer);
-            }
-            transaction.commit();
-        } catch (RuntimeException e) {
-            if (transaction != null && transaction.isActive()) {
-                transaction.rollback();
-            }
-            e.printStackTrace();
-            throw e;
-        } finally {
-            em.close();
-        }
-    }
+	@Override
+	public void deletePlayer(Player player) {
+		em = emf.createEntityManager();
+		EntityTransaction transaction = null;
+		try {
+			transaction = em.getTransaction();
+			if (transaction != null) {
+				transaction.begin();
+				Player managedPlayer = em.find(Player.class, player.getId());
+				if (managedPlayer != null) {
+					em.remove(managedPlayer);
+					transaction.commit();
+				} else {
+					throw new IllegalStateException("Tried to delete non existing player");
+				}
+			}else {
+				throw new IllegalStateException("Transaction is null");
+			}
+			
+		} catch (RuntimeException e) {
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+			e.printStackTrace();
+			throw e;
+		} finally {
+			em.close();
+		}
+	}
+	
 }
